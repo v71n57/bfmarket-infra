a24087ea3c6feb16439c091efc2a832d3208245b
Vtl <v71n57@gmail.com>
2022-07-01 20:17:41 +0300

    upd

---
+++ ansible/restore.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+vagrant snapshot restore master1 snap
+vagrant snapshot restore node1 snap
+vagrant snapshot restore node2 snap
---
+++ ansible/save.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+vagrant snapshot save master1 snap
+vagrant snapshot save node1 snap
+vagrant snapshot save node2 snap
